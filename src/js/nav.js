8ccdc5a41abab4477f73e726e6a6b996a176443f
Lendl63 <212922348+Lendl63@users.noreply.github.com>
2025-12-29 19:40:16 +0100

    feat: finalisation du header (navigation + menu berger

---
+++ src/js/nav.js
@@ -0,0 +1,54 @@
+/*===== Navigation avec nav du header =====*/
+
+/*===== Ouverture de la modal =====*/
+const openModal = () => {
+    const modalNav = document.querySelector('.modal-nav');
+    const navMenu = document.querySelector('.nav-menu');
+    navMenu.innerHTML = "&times;";
+    modalNav.classList.add('active');
+}
+
+/*===== Fermeture de la modal =====*/
+const closeModal = () => {
+    const modalNav = document.querySelector('.modal-nav');
+    const navMenu = document.querySelector('.nav-menu');
+    navMenu.innerHTML = "&#9776;";
+    modalNav.classList.remove('active');
+}
+
+const isDesktop = () => {
+    const width = window.innerWidth;
+    const navDiv = document.querySelector('.nav-div');
+    const navMenu = document.querySelector('.nav-menu');
+
+    if (width < 1025) {
+        navDiv.style.display = "none";
+        navMenu.style.display = "flex";
+    } else {
+        navDiv.style.display = "flex";
+        navMenu.style.display = "none";
+    }
+}
+
+/*===== Ajout de l'évenement au lien =====*/
+let count = 0; // Compter les click sur .nav-menu
+
+document.querySelector('.nav-menu').addEventListener('click', () => {
+    count++;
+    if (count === 1) {
+        openModal();
+    }
+    if (count === 2) {
+        closeModal();
+        count = 0;
+    }
+});
+const modalNavLinks = document.querySelectorAll('.modal-nav-link');
+
+modalNavLinks.forEach(link => {
+    link.addEventListener('click', closeModal);
+});
+
+/*===== Application du responsive (nav modal) sur tablette et modile =====*/
+window.addEventListener('resize', isDesktop);
+isDesktop();
